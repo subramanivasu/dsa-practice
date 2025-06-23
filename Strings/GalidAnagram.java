69972926f0489f2cf55cf30a9d7f033b063db771
subramanivasu <subramaniyanvasu@gmail.com>
2025-06-23 22:25:46 +0530

    Valid Anagram

---
+++ Strings/GalidAnagram.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class GalidAnagram {
+    public static boolean anagramStrings(String s, String t) {
+        //your code goes here
+        if(s.length()!=t.length()){
+            return false;
+        }
+        Map<Character,Integer> charecterFrequency = new HashMap<>();
+        
+        for(int i=0;i<s.length();i++){
+            charecterFrequency.put(s.charAt(i),charecterFrequency.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int i=0;i<t.length();i++){
+            charecterFrequency.put(t.charAt(i),charecterFrequency.getOrDefault(t.charAt(i),0)-1);
+        }
+    
+
+        for(Integer val : charecterFrequency.values()){
+            if(val!=0){
+                return false;
+            }
+        }
+
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(anagramStrings("anagram", "nagaram"));
+    }
+    
+}
