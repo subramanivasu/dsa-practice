49ceab151658e9d6797c3d94eee3b2b148de0818
subramanivasu <subramaniyanvasu@gmail.com>
2025-06-26 10:38:38 +0530

    Check if array is sorted using recursion

---
+++ recursion/CheckIfArrayIsSorted.java
@@ -0,0 +1,39 @@
+package recursion;
+import java.util.*;
+public class CheckIfArrayIsSorted {
+
+    public static boolean isSorted(ArrayList<Integer> nums) {
+        //your code goes here
+        boolean result;
+        
+
+        result = sorted(nums,0,1);
+
+        return result;
+
+    }
+
+    public static boolean sorted(ArrayList<Integer> nums,int left,int right){
+
+
+
+        if(left==nums.size()-1){
+            return true;
+        }
+
+        if(nums.get(left)<=nums.get(right)){
+            return sorted(nums,left+1,right+1);
+        } else{
+            return false;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isSorted(new ArrayList<>(List.of(1,2,3,4,5))));
+        System.out.println(isSorted(new ArrayList<>(List.of(11,4,2,6,4,2))));
+
+    }
+
+}
