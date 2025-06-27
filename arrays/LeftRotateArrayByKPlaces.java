a7282d18a5ccbd525568e4711759c72e26452725
subramanivasu <subramaniyanvasu@gmail.com>
2025-06-27 19:27:34 +0530

    Left Rotate Array by K places using iterative and optimal approach

---
+++ arrays/LeftRotateArrayByKPlaces.java
@@ -0,0 +1,76 @@
+package arrays;
+
+public class LeftRotateArrayByKPlaces {
+
+    public static void iterativeRotateArray(int[] nums, int k) {
+
+        int n = nums.length;
+
+        k = k % n;
+
+        int[] temp = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[i];
+        }
+
+        for (int i = k; i < nums.length; i++) {
+            nums[i - k] = nums[i];
+        }
+
+        for (int i = 0; i < k; i++) {
+            nums[n - k + i] = temp[i];
+        }
+    }
+
+    public static void optimalRotateArray(int[] nums, int k) {
+        int n = nums.length;
+            
+        k = k%n;
+
+
+        reverseArray(nums,0,k-1);
+        reverseArray(nums,k,n-1);
+        reverseArray(nums,0,n-1);
+    }
+
+        public void rotateArray(int[] nums, int k) {
+        int n = nums.length;
+
+        k = k % n;
+
+
+        reverseArray(nums, 0, k - 1);
+        reverseArray(nums, k, n - 1);
+        reverseArray(nums, 0, n - 1);
+    }
+
+    public static void reverseArray(int[] nums, int start, int end) {
+        while (start < end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{9,7,13,2,15,21};
+        iterativeRotateArray(nums,8);
+        for(int num:nums){
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+        
+        int[] nums1 = new int[]{9,7,13,2,15,21};
+
+        optimalRotateArray(nums1,8);
+        for(int num:nums){
+            System.out.print(num + " ");
+        }
+
+    }
+
+}
