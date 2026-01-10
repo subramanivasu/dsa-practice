104d8434e3fe363b2f75d909cd34b8fddf23329a
subramanivasu <subramaniyanvasu@gmail.com>
2026-01-10 22:33:05 +0530

    Added Sliding Window Maximum

---
+++ slidingwindow/SlidingWindowMaximum.java
@@ -0,0 +1,29 @@
+package slidingwindow;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class SlidingWindowMaximum {
+    public int[] maxSlidingWindow(int[] arr, int k) {
+        int n = arr.length;
+        int[] result = new int[n - k + 1];
+        int resultIndex = 0;
+        Deque<Integer> dq = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!dq.isEmpty() && dq.peekFirst() <= (i - k)) {
+                dq.removeFirst();
+            }
+            while (!dq.isEmpty() && arr[dq.peekLast()] <= arr[i]) {
+                dq.removeLast();
+            }
+            dq.offerLast(i);
+            if (i >= (k - 1)) {
+                result[resultIndex++] = arr[dq.peekFirst()];
+            }
+        }
+        return result;
+
+    }
+
+}
