1ec326f67690a4f35eacd2925cf8afa41a2ff8d1
subramanivasu <subramaniyanvasu@gmail.com>
2026-01-11 12:53:10 +0530

    Added Largest Rectangle in Histogram

---
+++ slidingwindow/LargestRectangleHistogram.java
@@ -0,0 +1,28 @@
+package slidingwindow;
+
+import java.util.Stack;
+
+public class LargestRectangleHistogram {
+    public int largestRectangleArea(int[] heights) {
+       
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+
+        int n = heights.length;
+
+        for (int i = 0; i <= n; i++) {
+            int currHeight = (i == n) ? 0 : heights[i];
+
+        while (!stack.isEmpty() && currHeight < heights[stack.peek()]) {
+            int height = heights[stack.pop()];
+            int right = i;
+            int left = (stack.isEmpty() ? -1 : stack.peek());
+            int area = height * (right - left - 1);
+            maxArea = Math.max(area, maxArea);
+        }
+        stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
